d8d7e3d9db1ed419242aacd510d7097dd508b82d
kdrkrtin <62345273+kdrkrtin@users.noreply.github.com>
2021-08-18 14:46:56 +0300

    Jquery ile çoklu dosya yükleme ve ön izleme

---
+++ multiple-file-preview/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $('#drag-and-drop-zone').on('dragenter', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).addClass('drag-active');
+    });
+    $('#drag-and-drop-zone').on('dragleave', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).removeClass('drag-active');
+    });
+    $('#drag-and-drop-zone').on('drop', function (e) {
+        if (e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
+            e.preventDefault();
+            e.stopPropagation();
+            /*UPLOAD FILES HERE*/
+            upload(e.originalEvent.dataTransfer.files);
+            $(this).removeClass('drag-active');
+        } else {
+            console.log('gorseller yuklenemedi')
+        }
+
+    });
+    function upload(files) {
+        var preview = $('#preview');
+
+        if (files) {
+            console.log(files);
+            [].forEach.call(files, readAndPreview);
+        }
+
+        function readAndPreview(file) {
+
+            // Make sure `file.name` matches our extensions criteria
+            if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
+                return alert(`${file.name}` + " uygun formatta bir dosya değil! Tekrar Deneyiniz.");
+            } else {
+                var reader = new FileReader();
+                reader.addEventListener("load", function () {
+                    preview.prepend(`
+                        <div class="wd-drop-item">
+                            <div class="wd-bg">
+                                <img src="` + this.result + `" title="` + file.name + `">
+                            </div>
+                        </div>
+                    `);
+                });
+                reader.readAsDataURL(file);
+            }
+        }
+    }
+})
